74953b5da143d0314fc203c6601407ed9022dbb6
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-08-22 23:06:24 +0300

    Add button to set grid per side and change BC div color if hovers.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Etch-aSketch project
---
+++ script.js
@@ -0,0 +1,47 @@
+const mainContainer = document.querySelector('.container')
+const boxes = document.querySelectorAll('.box')
+const gridSetter = document.getElementById('gridSetter')
+
+let gridSide = 16
+
+
+function createDivs(num){
+
+
+    for (let index = 0; index < (num * num); index++) {
+        appendDiv(num)
+    }
+}
+
+function calculateDivWidthAndHeight(number){
+    return (100 / number) +'%';
+}
+
+function appendDiv(num){
+    let newDiv = document.createElement('div');
+
+    newDiv.className = 'box'
+
+    newDiv.style.width = calculateDivWidthAndHeight(num);
+    newDiv.style.height = calculateDivWidthAndHeight(num);
+
+    addEventListenerToDiv(newDiv)
+    mainContainer.appendChild(newDiv);
+    
+
+}
+
+
+function addEventListenerToDiv (div){
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor =  'lightcoral';
+    })
+}
+
+gridSetter.addEventListener('click', function() {
+    gridSide = prompt('Set the grid side from 0 to 100')
+    createDivs(gridSide)
+})
+
+createDivs(16);
+
